f4362bdd03c9a5851ace38012a8c4da03cd71b52
Lyguh <deprofie082@gmail.com>
2025-03-06 12:08:56 +0300

    ЫЫЭ

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
-import {initNavigation} from "./modules/navigation.js";
-
+import initTestimonialSlider from "./modules/testimonial-slider.js";
+import { initNavigation } from "./modules/navigation.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    initNavigation()
+    initNavigation();
+    initTestimonialSlider();
 })
--- assets/scripts/modules/navigation.js
+++ assets/scripts/modules/navigation.js
@@ -1,39 +1,38 @@
 export function initNavigation() {
-    // Ищем элементы, с которыми будет взаимодействовать и добавляем их в переменные
+    // Ищем элементы, с которыми будет взаимодейвствовать и добавляем их в переменные
     const html = document.documentElement;
     const menu = document.querySelector('[data-menu]');
-    const menuToggler = document.querySelector('[data-menu-toggler]');
+    const menuToggler = document.querySelector('[data-menu-toggler]')
 
-    // Проверяем, существуют ли элементы, чтобы избежать ошибок
     if (!menu || !menuToggler) return;
 
     const toggleMenuState = () => {
         html.classList.toggle('is-lock');
         menu.classList.toggle('is-active');
-    };
+    }
 
     const closeOnOutsideClick = (e) => {
-        if (
-            menu.classList.contains('is-active') &&
-            !menu.contains(e.target) &&
-            !menuToggler.contains(e.target)
-        ) {
+        if(menu.classList.contains('is-active') && !menu.contains(e.target) && !menuToggler.contains(e.target)) {
             toggleMenuState();
         }
-    };
+    }
 
     const closeOnEsc = (e) => {
-        if (e?.key === 'Escape' && menu.classList.contains('is-active')) {
+        if (e.key === 'Escape' && menu.classList.contains('is-active')) {
             toggleMenuState();
         }
-    };
+    }
 
     const init = () => {
         menuToggler.addEventListener('click', toggleMenuState);
         document.addEventListener('click', closeOnOutsideClick);
         document.addEventListener('keydown', closeOnEsc);
-        console.log('Navigation initialized');
-    };
+        console.log('ok')
+    }
 
     init();
 }
+
+// Блокировака скролла на сайте
+// Открытие-закрытие меню
+// Закрытие при нажатии на клавишу esc
