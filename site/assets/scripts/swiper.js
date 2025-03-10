97ef1792d5a68328a63a8b711bb39c6671a91dea
arnd25 <and30206@gmail.com>
2025-03-10 19:17:06 +0300

    почти

--- site/assets/scripts/swiper.js
+++ site/assets/scripts/swiper.js
@@ -1,6 +1,4 @@
 const swiper = new Swiper('.swiper', {
-    loop: true,
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
